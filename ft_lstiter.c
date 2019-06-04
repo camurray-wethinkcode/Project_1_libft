2657e01d1f9a2a34da72e0441158bd463b32afec
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-04 17:44:00 +0200

    Adding ft_lstiter
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 17:37:10 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/04 17:43:13 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the lstiter function.
+** This function iterates through a list and applies
+** a function to each item in the list.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror ft_memcpy.c
+** ft_lstiter.c ft_lstnew.c ft_memalloc.c
+*/
 
 #include "libft.h"
 
@@ -11,3 +31,36 @@ void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 		lst = lst->next;
 	}
 }
+/*
+**void	iterfunct(t_list *v)
+**{
+**	*(size_t*)v->content = *(size_t*)v->content + 1;
+**}
+**
+**int		main(void)
+**{
+**	t_list	*begin;
+**	size_t	i;
+**	size_t	j;
+**
+**	i = 1;
+**	j = 2;
+**	begin = ft_lstnew(&i, sizeof(size_t));
+**	begin->next = ft_lstnew(&j, sizeof(size_t));
+**	ft_lstiter(begin, iterfunct);
+**	if (*(size_t*)begin->content != 2)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if (*(size_t*)begin->next->content != 3)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	free(begin->next);
+**	free(begin);
+**	write(1, "It works\n", 9);
+**	return (1);
+**}
+*/
