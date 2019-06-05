d8e60f281eaeb0c4dc93bb2e9c80df7917e84a3c
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-05 16:30:56 +0200

    Adding ft_memchr
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,15 @@
 /*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 16:03:58 by camurray          #+#    #+#             */
-/*   Updated: 2019/06/05 16:19:17 by camurray         ###   ########.fr       */
+/*   Updated: 2019/06/05 16:30:01 by camurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** Recreation of the memchr function.
 ** This function searches a memory block for a character.
+** In the main, the function rand() is a random
+** number generator used to generate input.
 ** The number 10000 that is multiplies in the main
 ** is the number of bits in a long data type.
 ** to test: uncomment main
