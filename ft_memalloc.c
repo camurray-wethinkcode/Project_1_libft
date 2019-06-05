f0b1229f2efabcf3bb1ca7df3a48ed4c9811d24d
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-05 10:59:23 +0200

    Adding ft_memalloc
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 10:52:56 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/05 10:57:36 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** 	Recreation of the memalloc function.
+** This function allocates a memory block fo specified length.
+** This function must return null if it fails to allocate memory.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_memalloc.c
+*/
 
 #include "libft.h"
 
@@ -19,3 +39,19 @@ void	*ft_memalloc(size_t size)
 	}
 	return (new);
 }
+/*
+**int		main(void)
+**{
+**	void		*ret;
+**
+**	ret = ft_memalloc(4);
+**	ft_memdel(&ret);
+**	if (ret != NULL)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (1);
+**}
+*/
