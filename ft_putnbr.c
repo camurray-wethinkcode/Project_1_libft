44f241d02314d5c34ffd0b7624284f044438704b
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-05 17:04:43 +0200

    Adding ft_putnbr
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,31 +6,32 @@
 /*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:15:54 by camurray          #+#    #+#             */
-/*   Updated: 2019/05/28 12:13:26 by camurray         ###   ########.fr       */
+/*   Updated: 2019/06/05 17:03:45 by camurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** print int n on standard output
+** Recreation fo the putnbr function.
+** This function prints int n on the standard output.
 ** to test: uncomment main
-** run gcc -Wall -Wextra -Werror
+** run gcc -Wall -Wextra -Werror ft_strlen.c
 ** ft_putnbr.c ft_putchar.c ft_putstr.c
 */
 
 #include "libft.h"
 
-void    ft_putnbr(int n)
+void	ft_putnbr(int n)
 {
-    if (n == -2147483648)
-        return (ft_putstr("-2147483648"));
-    if (n < 0)
-    {
-        n *= -1;
-        ft_putchar('-');
-    }
-    if (n > 9)
-        ft_putnbr(n / 10);
-    ft_putchar(n % 10 + 48);
+	if (n == -2147483648)
+		return (ft_putstr("-2147483648"));
+	if (n < 0)
+	{
+		n *= -1;
+		ft_putchar('-');
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + '0');
 }
 /*
 **int		main(void)
