ce3a19a3718ff4cf76ee9c2e9c5a82516f03da2f
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-05 17:19:46 +0200

    Adding ft_strcmp
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,7 +1,26 @@
-//#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 17:14:05 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/05 17:19:06 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strcmp function.
+** This function compares two strings lexicographically.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strcmp.c
+*/
+
 #include "libft.h"
 
-int		ft_strcmp(char const *s1, char const *s2)
+int		ft_strcmp(const char *s1, const char *s2)
 {
 	while (*s1 == *s2)
 	{
@@ -10,14 +29,17 @@ int		ft_strcmp(char const *s1, char const *s2)
 		if (*s1 == 0 && *s2 == 0)
 			return (0);
 	}
-	return  * (unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1;
+	return (*((unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1));
 }
 /*
-int		main(void)
-{
-	printf("%d\n", ft_strcmp("cba", "abc"));
-	printf("%d\n", strcmp("cba", "abc"));
-	printf("%d\n", ft_strcmp("a", "abcde"));
-	printf("%d\n", strcmp("a", "abcde"));
-}
+**int		main(void)
+**{
+**	write(1, "The two numbers should match in all 3 cases\n", 44);
+**	printf("%d\n", ft_strcmp("cba", "abc"));
+**	printf("%d\n", strcmp("cba", "abc"));
+**	printf("%d\n", ft_strcmp("a", "abcde"));
+**	printf("%d\n", strcmp("a", "abcde"));
+**	printf("%d\n", ft_strcmp("abc", "abc"));
+**	printf("%d\n", strcmp("abc", "abc"));
+**}
 */
