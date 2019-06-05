4ea32834261dbf893548793fc528f4c09294c359
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-05 17:40:41 +0200

    Adding ft_strclr
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strclr.c
+++ ft_strclr.c
@@ -1,3 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strclr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 17:37:04 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/05 17:39:52 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation fo the strclr function.
+** This function clears a string.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strclr.c
+*/
 
 #include "libft.h"
 
@@ -10,3 +29,25 @@ void	ft_strclr(char *s)
 		while (s[i])
 			s[i++] = '\0';
 }
+/*
+**int		main(void)
+**{
+**	int			i;
+**	char		str[] = "Hello";
+**
+**	i = 0;
+**	ft_strclr(NULL);
+**	ft_strclr(str);
+**	while (i < 6)
+**	{
+**		if (str[i] != '\0')
+**		{
+**			write(1, "It doesn't work\n", 16);
+**			return (0);
+**		}
+**	i = i + 1;
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
