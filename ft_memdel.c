43322927812d9a09815aefcdf82e908c4c001a58
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-05 11:12:23 +0200

    Adding ft_memdel
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_memdel.c
+++ ft_memdel.c
@@ -1,3 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 11:05:09 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/05 11:11:22 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the memdel function.
+** This function frees a memory block.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_memdel.c
+*/
 
 #include "libft.h"
 
@@ -11,3 +30,19 @@ void	ft_memdel(void **ap)
 		*ap = NULL;
 	}
 }
+/*
+**int		main(void)
+**{
+**	void		*ret;
+**
+**	ret = ft_memalloc(4);
+**	ft_memdel(&ret);
+**	if (ret != NULL)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
