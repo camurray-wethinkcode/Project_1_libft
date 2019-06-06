7033348c0e9d23ff72f49f8f22b6b8b8b2808f43
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:02:40 +0200

    Addinf ft_strdup
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 09:56:26 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:01:44 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strdup function.
+** This function duplicates a string with a new
+** memory block and does not overwrite anything.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strdup.c
+*/
 
 #include "libft.h"
 
-char	*ft_strdup(char *str)
+char	*ft_strdup(const char *s1)
 {
-	char	*s;
+	char	*str;
 	int		i;
 
 	i = 0;
-	while (str[i])
+	while (s1[i])
 		i++;
-	s = (char *)malloc(sizeof(char) * (i + 1));
-	if (s == 0)
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (str == 0)
 		return (NULL);
 	i = 0;
-	while (str[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		s[i] = str[i];
+		str[i] = s1[i];
 		i++;
 	}
-	s[i] = '\0';
-	return (s);
+	str[i] = '\0';
+	return (str);
 }
+/*
+**int		main(void)
+**{
+**	char		*ret1;
+**	char		*ret2;
+**
+**	ret1 = strdup("");
+**	ret2 = ft_strdup("");
+**	if (strcmp(ret1, ret2) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	free(ret1);
+**	free(ret2);
+**	ret1 = strdup("hello");
+**	ret2 = ft_strdup("hello");
+**	if (strcmp(ret1, ret2) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	free(ret1);
+**	free(ret2);
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
