462abaa0f76c44ef8e0fec5457c93bcdab571d8b
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:38:33 +0200

    Adding ft_strlcat
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,22 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:29:13 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:37:58 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strlcat function.
+** This function is a better alternative than strncat
+** as it concatenates input in a less error prone manner.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strlcat.c ft_strlen.c ft_memset.c
+*/
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *s1, const char *s2, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	len;
 
 	i = 0;
 	len = 0;
-	while (s1[len] && len < size)
+	while (dst[len] && len < dstsize)
 		len++;
 	i = len;
-	while (s2[len - i] && len + 1 < size)
+	while (src[len - i] && len + 1 < dstsize)
 	{
-		s1[len] = s2[len - i];
+		dst[len] = src[len - i];
 		len++;
 	}
-	if (i < size)
-		s1[len] = '\0';
-	return (i + ft_strlen(s2));
+	if (i < dstsize)
+		dst[len] = '\0';
+	return (i + ft_strlen(src));
 }
+/*
+**int		main(void)
+**{
+**	char		dst1[50] = {0};
+**	char		dst2[50] = {0};
+**	size_t		i;
+**	size_t		j;
+**	size_t		k;
+**
+**	if (strlcat(dst1, "Hello ", 4) != ft_strlcat(dst2, "Hello ", 4))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	ft_memset(dst1, 0, sizeof(dst1));
+**	ft_memset(dst2, 0, sizeof(dst1));
+**	j = strlcat(dst1, "Hello ", 4);
+**	k = ft_strlcat(dst2, "Hello ", 4);
+**	if (strcmp(dst1, dst2) != 0 || j != k)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	j = strlcat(dst1, "Hello ", 1);
+**	k = ft_strlcat(dst2, "Hello ", 1);
+**	if (strcmp(dst1, dst2) != 0 || j != k)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	i = 0;
+**	while (i < 6)
+**	{
+**		dst1[4 + i] = 'z';
+**		dst2[4 + i] = 'z';
+**		++i;
+**	}
+**	j = strlcat(dst1, "abc", 6);
+**	k = ft_strlcat(dst2, "abc", 6);
+**	if (strcmp(dst1, dst2) != 0 || j != k)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
