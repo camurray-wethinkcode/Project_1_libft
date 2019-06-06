3b4705584b5d5d2b91cf6feb003607c8569e81b9
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:23:01 +0200

    Adding ft_striteri
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:18:45 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:22:18 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the striteri function.
+** This function applies function f to every
+** character in the string from an index given.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_striteri.c
+*/
 
 #include "libft.h"
 
@@ -15,3 +35,25 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		j++;
 	}
 }
+/*
+**void	iteration(unsigned int i, char *s)
+**{
+**	*s = *s + i;
+**}
+**
+**int		main(void)
+**{
+**	char		str[] = "Hello";
+**
+**	ft_striteri(NULL, NULL);
+**	ft_striteri(str, NULL);
+**	ft_striteri(str, iteration);
+**	if (strcmp(str, "Hfnos") != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
