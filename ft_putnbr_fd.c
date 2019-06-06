dfc47e9c5d012c53e739a93abf4e1874015fc255
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 12:01:13 +0200

    Adding ft_putnbr_fd
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,22 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:59:28 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 12:00:19 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the putnbr_fd function.
+** The same as putnbr except we ouput to a given
+** file descriptor instead of to the standard output.
+*/
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int nb, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int n;
+	unsigned int i;
 
-	n = 0;
-	if (nb < 0)
+	i = 0;
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		nb = -nb;
+		n = -n;
 	}
-	n = (unsigned int)nb;
-	if (n < 10)
-		ft_putchar_fd(n + 48, fd);
+	i = (unsigned int)n;
+	if (i < 10)
+		ft_putchar_fd(i + 48, fd);
 	else
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(n % 10 + 48, fd);
+		ft_putnbr_fd(i / 10, fd);
+		ft_putchar_fd(i % 10 + 48, fd);
 	}
 }
