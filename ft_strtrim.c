94824329cd1ead36218409547b12aec1f69230bb
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 09:32:16 +0200

    Adding ft_strtrim
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 09:23:05 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 09:31:37 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strtrim function.
+** This function trims whitespaces from the start
+** and end of the given string (' ', \n, \t).
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror ft_strnew.c
+** ft_strtrim.c ft_strlen.c ft_strsub.c
+*/
 
 #include "libft.h"
 
@@ -16,3 +36,40 @@ char	*ft_strtrim(char const *s)
 		new--;
 	return (ft_strsub(s, 0, new - s + 1));
 }
+/*
+**int		main(void)
+**{
+**	char			str1[] = "  \t    \t\nBon\t \njour\t\n  \n     ";
+**	char			str2[] = "Bonjour";
+**	char			str3[] = "  \t\t\t   ";
+**	char			*r;
+**
+**	ft_strtrim(NULL);
+**	r = ft_strtrim(str1);
+**	if (strcmp(r, "Bon\t \njour"))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		free(r);
+**		return (0);
+**	}
+**	free(r);
+**	r = ft_strtrim(str2);
+**    if (strcmp(r, "Bonjour") || (str2 == r))
+**    {
+**        write(1, "It doesn't work\n", 16);
+**		free(r);
+**        return (0);
+**    }
+**    free(r);
+**	r = ft_strtrim(str3);
+**    if (strcmp(r, "") || (str3 == r))
+**    {
+**        write(1, "It doesn't work\n", 16);
+**		free(r);
+**       return (0);
+**    }
+**    free(r);
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
