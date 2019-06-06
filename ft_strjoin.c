81b5448e44298793f46f70a19464dcb0cc50cd5a
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:29:02 +0200

    Adding ft_strjoin
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,7 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:23:15 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:28:10 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strjoin function.
+** This function joins multiple strings together
+** without overwriting any string by creating
+** a new null terminated memory block.
+** If the memory allocation fails this
+** function returns null.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strjoin.c ft_strlen.c
+*/
 
 #include "libft.h"
 
-char	*ft_strjoin(char	const *s1, char	const *s2)
+char	*ft_strjoin(char const *s1, char	const *s2)
 {
 	char	*new;
 	size_t	i;
@@ -28,3 +51,47 @@ char	*ft_strjoin(char	const *s1, char	const *s2)
 	new[i] = '\0';
 	return (new);
 }
+/*
+**int		main(void)
+**{
+**	char		*ret;
+**
+**	ft_strjoin(NULL, NULL);
+**	ft_strjoin(NULL, "");
+**	ft_strjoin("", NULL);
+**	ret = ft_strjoin("Hello ", "boys");
+**	if (strcmp(ret, "Hello boys") != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		free(ret);
+**		return (0);
+**	}
+**	free(ret);
+**	ret = ft_strjoin("", "boys");
+**    if (strcmp(ret, "boys") != 0)
+**    {
+**		write(1, "It doesn't work\n", 16);
+**        free(ret);
+**        return (0);
+**    }
+**    free(ret);
+**	ret = ft_strjoin("Hello ", "");
+**    if (strcmp(ret, "Hello ") != 0)
+**    {
+**		write(1, "It doesn't work\n", 16);
+**        free(ret);
+**        return (0);
+**    }
+**    free(ret);
+**	ret = ft_strjoin("", "");
+**    if (strcmp(ret, "") != 0)
+**    {
+**		write(1, "It doesn't work\n", 16);
+**        free(ret);
+**        return (0);
+**    }
+**    free(ret);
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
