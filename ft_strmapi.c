e4ebfddaabd9f1278d943fb80222726c5c214b74
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 11:00:35 +0200

    Adding ft_strmapi
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:55:55 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:59:42 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strmapi function.
+** This function is the same as strmap
+** but has an added index.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strmapi.c ft_strlen.c ft_strnew.c
+*/
 
 #include "libft.h"
 
@@ -19,3 +39,29 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	}
 	return (new);
 }
+/*
+**char	iteration(unsigned int i, char s)
+**{
+**	return (s + i);
+**}
+**
+**int		main(void)
+**{
+**	char		str[] = "Hello";
+**	char		empty[] = "";
+**	char		*ret;
+**
+**	ft_strmapi(NULL, NULL);
+**	ft_strmapi(empty, NULL);
+**	ret = ft_strmapi(str, iteration);
+**	if (strcmp(ret, "Hfnos") != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		free(ret);
+**		return (0);
+**	}
+**	free(ret);
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
