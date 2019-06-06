f6b4946b374e9766331a9c9109e0cb18a7be5961
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 11:24:51 +0200

    Adding ft_strnew
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strnew.c
+++ ft_strnew.c
@@ -1,3 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:20:48 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 11:24:13 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strnew function.
+** This function creates a new null terminated string.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strnew.c
+*/
 
 #include "libft.h"
 
@@ -20,3 +39,27 @@ char	*ft_strnew(size_t size)
 	}
 	return (f);
 }
+/*
+**int		main(void)
+**{
+**	char		*ret;
+**	int			i;
+**
+**	i = 0;
+**	ret = ft_strnew(4);
+**	if (ret != NULL)
+**	{
+**		while (i < 5)
+**		{
+**			if (ret[i] != '\0')
+**			{
+**				write(1,"It doesn't work\n", 16);
+**				return (0);
+**			}
+**			i = i + 1;
+**		}
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
