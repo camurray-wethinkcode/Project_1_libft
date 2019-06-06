4f612d8616346d9b66294b7b9e909b82946c9d90
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:54:33 +0200

    Adding ft_strmap
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strmap.c
+++ ft_strmap.c
@@ -1,3 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:45:39 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:53:57 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strmap function.
+** This function is similar to striter
+** except it doesn't modify the original
+** string but allocates a memory block
+** to store a new null terminated string.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strmap.c ft_strlen.c ft_strnew.c
+*/
 
 #include "libft.h"
 
@@ -19,3 +41,28 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	}
 	return (new);
 }
+/*
+**char	iteration(char s)
+**{
+**	return (s + 1);
+**}
+**
+**int		main(void)
+**{
+**	char		str[] = "Hello";
+**	char		*ret;
+**
+**	ft_strmap(NULL, NULL);
+**	ft_strmap("", NULL);
+**	ret = ft_strmap(str, iteration);
+**	if (strcmp(ret, "Ifmmp") != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		free(ret);
+**		return (0);
+**	}
+**	free(ret);
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
