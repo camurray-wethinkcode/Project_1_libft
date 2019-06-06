5c8abfd78eac9f2ec25426bc368bbefb7cd06c41
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 11:59:13 +0200

    Adding ft_putchar_fd
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,3 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:56:43 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 11:57:32 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the putchar_fd function.
+** The same as putchar except we output to a
+** file descriptor instead of to the standard output.
+*/
 
 #include "libft.h"
 
