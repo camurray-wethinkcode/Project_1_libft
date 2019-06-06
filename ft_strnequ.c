b55fe82092fe77451e1944ab14a91af058896c0f
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 11:20:35 +0200

    Adding ft_strnequ
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -1,3 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:16:40 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 11:19:55 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strnequ function.
+** The same as strequ but has an added index.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strnequ.c ft_strncmp.c
+*/
 
 #include "libft.h"
 
@@ -10,3 +29,42 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	else
 		return (0);
 }
+/*
+**int		main(void)
+**{
+**	int			ret;
+**
+**	ft_strnequ(NULL, NULL, 0);
+**	ft_strnequ(NULL, NULL, 1);
+**	ft_strnequ(NULL, "", 1);
+**	ft_strnequ("", NULL, 1);
+**	ret = 0;
+**	if ((ret = ft_strnequ("abc", "abc", 2)) != 1)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strnequ("cba", "abc", 2)) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strnequ("abc", "cba", 2)) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strnequ("abc", "abd", 2)) != 1)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strnequ("", "", 3)) != 1)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
