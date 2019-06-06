a48dac95d08b3698bbe1403d1dc5a7da85c8ccd4
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 11:43:55 +0200

    Adding ft_strsub
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strsub.c
+++ ft_strsub.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:39:40 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 11:43:23 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strsub function.
+** This function cuts a string out of a string
+** and returns it in a newly allocated memory block.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strsub.c
+*/
 
 #include "libft.h"
 
@@ -20,3 +40,29 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	new[i] = '\0';
 	return (new);
 }
+/*
+**int		main(void)
+**{
+**	char		*ret;
+**	char		str[] = "*Hello*";
+**
+**	ft_strsub(NULL, 0, 0);
+**	ft_strsub(NULL, 1, 0);
+**	ft_strsub(NULL, 1, 1);
+**	ft_strsub(NULL, 0, 1);
+**	ret = ft_strsub(str, 1, 5);
+**	if (strcmp(ret, "Hello") != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	ret = ft_strsub(str, 3, 1);
+**    if (strcmp(ret, "l") != 0)
+**    {
+**		write(1, "It doesn't work\n", 16);
+**        return (0);
+**    }
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
