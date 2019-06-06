6b9b8f05a77cd8f17b2890beb7be22a971f3c0eb
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:12:19 +0200

    Adding ft_strequ
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strequ.c
+++ ft_strequ.c
@@ -1,3 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strequ.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:03:43 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:11:40 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strequ function.
+** This function lexicographically compares two strings
+** to test if they are identical.
+** In the main function the line
+** str = strdup("abc") fixes a false positive
+** for if s1 == s2 retun 1.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strequ.c ft_strlen.c ft_strcmp.c
+*/
 
 #include "libft.h"
 
@@ -10,3 +33,51 @@ int		ft_strequ(char const *s1, char const *s2)
 	else
 		return (0);
 }
+/*
+**int		main(void)
+**{
+**	int			ret;
+**	char		*str;
+**
+**	ft_strequ(NULL, NULL);
+**	ft_strequ("", NULL);
+**	ft_strequ(NULL, "");
+**	ret = 0;
+**	str = strdup("abc");
+**
+**	if ((ret = ft_strequ(str, "abc")) != 1)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		free(str);
+**		return (0);
+**	}
+**	free(str);
+**	if ((ret = ft_strequ("cba", "abc")) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strequ("abc", "cba")) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strequ("", "")) != 1)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strequ("abc", "abcd")) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if ((ret = ft_strequ("abcd", "abc")) != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
