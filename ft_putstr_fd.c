5cab790b6260a68e23f3744f33ee3012e829ea54
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 12:04:39 +0200

    Adding ft_putstr_fd
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,4 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:55:06 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 11:56:01 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+/*
+** Recreation of the putstr_fd function.
+** The same as putstr except we output
+** to a file descriptor instead of to the
+** standard output.
+*/
 
 #include "libft.h"
 
