4cb8bc8a1d6356dd555c529db4b34fd7f71afd26
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 10:18:36 +0200

    Adding ft_striter
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_striter.c
+++ ft_striter.c
@@ -1,3 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:12:31 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 10:17:55 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the striter function.
+** This function iterates over a string and applies
+** function f to every character in the string.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_striter.c
+*/
 
 #include "libft.h"
 
@@ -14,3 +34,25 @@ void	ft_striter(char *s, void (*f)(char *))
 		j++;
 	}
 }
+/*
+**void	iteration(char *s)
+**{
+**	*s = *s + 1;
+**}
+**
+**int		main(void)
+**{
+**	char		str[] = "Hello";
+**
+**	ft_striter(NULL, NULL);
+**	ft_striter(str, NULL);
+**	ft_striter(str, iteration);
+**	if (strcmp(str, "Ifmmp") != 0)
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
