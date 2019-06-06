7e3f71dfcf9a0851519449c03a3d2f513dd57bc2
camurray-wethinkcode <50704195+camurray-wethinkcode@users.noreply.github.com>
2019-06-06 11:31:59 +0200

    Adding ft_strnstr
    Added commentary to describe what the function does.
    Added a logic map run through of the code.
    Added testing instructions.
    Added a main to test the code with.
    Checked that the file passes norminette.
    DONE!

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,29 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camurray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 11:24:59 by camurray          #+#    #+#             */
+/*   Updated: 2019/06/06 11:31:29 by camurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Recreation of the strnstr function.
+** This function is the smae as strstr but
+** has an added index.
+** to test: uncomment main
+** run gcc -Wall -Wextra -Werror
+** ft_strnstr.c
+*/
 
 #include "libft.h"
 
-char	*ft_strnstr(char *str, char *to_find, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	unsigned int	count;
-	int				i;
-	int				j;
+	unsigned int i;
+	unsigned int j;
 
-	count = 0;
+	if (!*needle)
+		return ((char*)haystack);
 	i = 0;
-	if (to_find[i] == '\0')
-		return (str);
-	while (to_find[count])
-		count++;
-	while (str[i] && len >= count)
+	while (haystack[i] != '\0' && (size_t)i < len)
 	{
-		j = 0;
-		while (str[i + j] == to_find[j])
+		if (haystack[i] == needle[0])
 		{
-			j++;
-			if (to_find[j] == '\0')
-				return (&str[i]);
+			j = 1;
+			while (needle[j] != '\0' && haystack[i + j] == needle[j] &&
+					(size_t)(i + j) < len)
+				++j;
+			if (needle[j] == '\0')
+				return ((char*)&haystack[i]);
 		}
-		i++;
-		len--;
+		++i;
 	}
-	return (NULL);
+	return (0);
 }
+/*
+**int		main(void)
+**{
+**	char		*str = "Hello you peep";
+**
+**
+**	if (strnstr(str, "Hello", 6) != ft_strnstr(str, "Hello", 6))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if (strnstr(str, "Hello", 3) != ft_strnstr(str, "Hello", 3))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if (strnstr(str, "you", 1) != ft_strnstr(str, "you", 1))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if (strnstr(str, "pee", 2) != ft_strnstr(str, "pee", 2))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if (strnstr(str, "p", 0) != ft_strnstr(str, "p", 0))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	if (strnstr(str, "", 3) != ft_strnstr(str, "", 3))
+**	{
+**		write(1, "It doesn't work\n", 16);
+**		return (0);
+**	}
+**	write(1, "It works\n", 9);
+**	return (0);
+**}
+*/
